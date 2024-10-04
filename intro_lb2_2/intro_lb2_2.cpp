e606f7a4072ea6228a126b14a9bc4277bc7cd917
Nikita-Parus <cheems.nz@gmail.com>
2024-10-04 19:09:35 +0300

    Добавьте файлы проекта.

---
+++ intro_lb2_2/intro_lb2_2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int year_1, year_2, tenths = 0;
+    cin >> year_1 >> year_2;
+    for (int i = 1; i < (year_2-year_1); i++) {
+        if (i > 100) break;
+        if (i % 10 == 0) tenths++;
+        cout << i + year_1 << endl;
+    }
+    cout << "Number of 10th elements: " << tenths << endl;
+}
